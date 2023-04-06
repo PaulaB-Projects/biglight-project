57f8a186b83817b62fcb75f38a511b47495ce915
Paula Burghelea <paulaburghelea.github@gmail.com>
2023-04-06 12:36:36 +0100

    uploading all work done on the JS and scss fileso far

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+window.addEventListener("DOMContentLoaded", (event) => { 
+	
 const productList = [...document.getElementsByClassName('product-card')];
 const [card1, card2, ...other] = productList;
 const promoMessage = "Promo text offer";
@@ -11,18 +13,71 @@ const insertDiv = (element, className = "") => {
 	return promoDiv;
 }
 
-window.addEventListener("DOMContentLoaded", (event) => { 
-	if ( productList.length > 1 )
+
+if ( productList.length > 1 )
 	insertDiv(card2, "promo")
-	// const promoDiv = card2.parentNode.insertBefore(document.createElement('div'), card2.nextSibling);
-	// promoDiv.innerHTML = '<p>Promo Message</p>';
-	// promoDiv.style.backgroundColor = "red";
-	else if (productList.length != 0) //could not check truthiness for the love of me 
+else if (productList.length != 0) //could not check truthiness for the love of me 
 	insertDiv(card1, "promo");
-	// const promoDiv = card1.parentNode.insertBefore(document.createElement('div'), card1.nextSibling);
-	// promoDiv.innerHTML = '<p>Promo Message</p>';
-	// promoDiv.style.backgroundColor = "red";
-	else 
+else 
 	console.error('promo message not displayed since there are no products available');
-	
+
 });
+
+
+// window.addEventListener("load", (event) => {
+
+// 	const productList = [...document.getElementsByClassName('product-card')];
+// 	const [card1, card2, ...other] = productList;
+// 	const promoMessage = "Promo text offer";
+// 	const promoCode = "ofuayrgd";
+	  
+// 	//helper functions
+// 	const insertDiv = (element, className = "") => {
+// 		const promoDiv = element.parentNode.insertBefore(document.createElement('div'), element.nextSibling);
+// 		promoDiv.classList.add(`${className}`);
+// 		promoDiv.innerHTML = `<p> ${promoMessage}: ${promoCode} </p>`;
+// 		return promoDiv;
+// 	}
+	
+// 	if ( productList.length > 1 )
+// 		insertDiv(card2, "promo")
+// 		// const promoDiv = card2.parentNode.insertBefore(document.createElement('div'), card2.nextSibling);
+// 		// promoDiv.innerHTML = '<p>Promo Message</p>';
+// 		// promoDiv.style.backgroundColor = "red";
+// 	else if (productList.length != 0) //could not check truthiness for the love of me 
+// 		insertDiv(card1, "promo");
+// 		// const promoDiv = card1.parentNode.insertBefore(document.createElement('div'), card1.nextSibling);
+// 		// promoDiv.innerHTML = '<p>Promo Message</p>';
+// 		// promoDiv.style.backgroundColor = "red";
+// 	else 
+// 		console.error('promo message not displayed since there are no products available');
+// 	});
+	
+// 	document.addEventListener("click", (event) => {
+	
+// 	const productList = [...document.getElementsByClassName('product-card')];
+// 	const [card1, card2, ...other] = productList;
+// 	const promoMessage = "Promo text offer";
+// 	const promoCode = "ofuayrgd";
+	  
+// 	//helper functions
+// 	const insertDiv = (element, className = "") => {
+// 		const promoDiv = element.parentNode.insertBefore(document.createElement('div'), element.nextSibling);
+// 		promoDiv.classList.add(`${className}`);
+// 		promoDiv.innerHTML = `<p> ${promoMessage}: ${promoCode} </p>`;
+// 		return promoDiv;
+// 	}
+	
+// 	if ( productList.length > 1 )
+// 		insertDiv(card2, "promo")
+// 		// const promoDiv = card2.parentNode.insertBefore(document.createElement('div'), card2.nextSibling);
+// 		// promoDiv.innerHTML = '<p>Promo Message</p>';
+// 		// promoDiv.style.backgroundColor = "red";
+// 	else if (productList.length != 0) //could not check truthiness for the love of me 
+// 		insertDiv(card1, "promo");
+// 		// const promoDiv = card1.parentNode.insertBefore(document.createElement('div'), card1.nextSibling);
+// 		// promoDiv.innerHTML = '<p>Promo Message</p>';
+// 		// promoDiv.style.backgroundColor = "red";
+// 	else 
+// 		console.error('promo message not displayed since there are no products available');
+// 	});
